f379f02068982da787df3b549df407387b553fb3
merelee <84946124+merelee@users.noreply.github.com>
2023-09-04 17:16:08 -0400

    update exported .csv format

--- champ_data.py
+++ champ_data.py
@@ -5,7 +5,7 @@ import pprint
 
 #build champ url
 league_version = '13.17.1'
-champ_name = 'Malphite'
+champ_name = 'Yone'
 champ_url = 'http://ddragon.leagueoflegends.com/cdn/' + league_version + '/data/en_US/champion/' + champ_name + '.json'
 
 #get json data
@@ -15,22 +15,20 @@ if json_data.ok:
     
     #grab champ info
     champ_info = json_data.json()['data'][champ_name]
-    #champ_info includes: allytips, blurb, enemytips, id, image, info, key, lore, name, partype, passive, recommended, skins, spells, stats, tags, title
+    #included dictionaries: allytips, blurb, enemytips, id, image, info, key, lore, name, partype, passive, recommended, skins, spells, stats, tags, title
     #pprint.pprint(champ_info)
     
     #create the csv
     csv_filename = champ_name + '_data.csv'
 
     with open(csv_filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-    
-        #create headers
-        writer.writerow(['stat', 'value'])
-    
+        
         #grab stats
-        stats_data = champ_info.get('stats', {})
-        for key, value in stats_data.items():
-            writer.writerow([key, value])
+        stats_data = champ_info['stats']
+        headers = [key for key in stats_data.keys()]
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        writer.writerow({key: value for key, value in stats_data.items()})
             
         print(f'Successfully created {csv_filename}')
 
