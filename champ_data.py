82268d58cdc1246a286d03d40948cf8aadb9f080
merelee <84946124+merelee@users.noreply.github.com>
2023-09-05 04:33:44 -0400

    automatically check ddragon version
    - user no longer needs to manually edit league_version variable
    
    - added ddragon version to exported csv file name (instead of champion_data.csv, it is now champion_data_###.csv)

--- champ_data.py
+++ champ_data.py
@@ -2,10 +2,11 @@ import requests
 import json
 import csv
 
-#build champ url
-league_version = '13.17.1'
-base_url = 'http://ddragon.leagueoflegends.com/cdn/' + league_version + '/data/en_US/champion/'
-    
+#get data dragon version, and build base champ url
+versions_file = requests.get('https://ddragon.leagueoflegends.com/api/versions.json')
+version_check = versions_file.json()[0]
+base_url = 'http://ddragon.leagueoflegends.com/cdn/' + version_check + '/data/en_US/champion/'
+
 #get list of champions
 with open('champions.json', 'r') as file:
     data = json.load(file)
@@ -13,7 +14,7 @@ with open('champions.json', 'r') as file:
 champ_list = data['champions']
 
 #create the csv
-csv_filename = 'champion_data.csv'
+csv_filename = 'champion_data_' + version_check + '.csv'
 print(f'Creating {csv_filename}...')
 
 with open(csv_filename, 'w', newline='') as csvfile:
@@ -35,7 +36,7 @@ with open(csv_filename, 'w', newline='') as csvfile:
             #first champ
             if writer is None:
                 stats_data = champ_info['stats']
-                headers = ['name'] + [key for key in stats_data.keys()] #writes headers (names, then stats)
+                headers = ['name'] + [key for key in stats_data.keys()] #writes headers (name, then stats)
                 writer = csv.DictWriter(csvfile, fieldnames=headers)
                 writer.writeheader()
 
@@ -48,6 +49,6 @@ with open(csv_filename, 'w', newline='') as csvfile:
             error = True
             
     if error:
-        print (f'Error adding champ data. Make sure the league_version is correct.\nleague_version: {league_version}')
+        print ('There was an error adding champion data.')
 
 print(f'Successfully created {csv_filename}')
